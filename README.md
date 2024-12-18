9f0d18a20349fc20bd70385ad7b44a840fcc82ef
Rohan Chandrashekhar <rohanc8232@gmail.com>
2024-12-18 07:05:54 +0530

    Added structure of folder

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Medical-chatbot
+
+# Steps to run:
+Clone the repository
+
+'''bash
+Project repo: https://github.com/
+'''
+
+'''bash
+conda create -n medicalbot python=3.10 -y
+'''
+
+'''bash
+conda activate medicalbot
+'''
+
+'''bash
+pip install -r requirements.txt
+'''
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup # type: ignore
+
+setup(
+    name = 'Medical chatbot',
+    version = '0.0.0',
+    author = 'Rohan Chandrashekhar',
+    author_email = 'rohanc8232@gmail.com',
+    packages = find_packages(),
+    install_requires=[]
+)
